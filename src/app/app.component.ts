356f737c52eb7e6b2d544941c59a534371fe7425
Aracno-mac <aracno-mac@MacBook-Pro-de-Aracno-mac.local>
2017-06-09 12:23:52 +0200

    View para el login asincronico , mientras comporbamos si estamos  autentificados o no  y decide hacia donde mandarnos si al login o al home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,46 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { AuthProvider } from '../providers/auth/auth';
+
+import { LoadingController } from 'ionic-angular';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+  rootPage: any = LoginPage;
+  loader: any;
+
+  constructor(public auth: AuthProvider, public loadingCtrl: LoadingController){ 
+
+    this.presentLoading();
+
+    this.auth.login().then((isLoggedIn) => {
+
+      if(isLoggedIn){
+        this.rootPage = HomePage
+      } else {
+        this.rootPage = LoginPage;
+      }
+
+      this.loader.dismiss();
+
     });
   }
+
+  presentLoading() {
+
+    this.loader = this.loadingCtrl.create({
+      content: "Autentificando ...",
+    });
+
+    this.loader.present();
+
+  }
+
 }
 
